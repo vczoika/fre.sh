dad318e7f91d8acf4198556acdff539728b157d9
vczoika <viniciusc.doom@gmail.com>
2020-10-07 03:19:20 -0300

    Added new information to main.rs

--- fre.sh
+++ fre.sh
@@ -1,1 +1,6 @@
-sudo apt-get update
+chmod +x fre.sh
+    echo "Hello friend. Hello friend? That's lame. Maybe I should give you a name."
+    sleep 3
+    yes | sudo apt-get update
+    sudo apt-get upgrade
+    echo "Goodbye, friend. That's lame. We're talking to an imaginary person. Just like we always have."
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+// This main file gets all the information needed.
+// When run, it creates a new file called fre.sh
+// Every cargo run WILL OVERWRITE the file with new information.
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
     let mut file = File::create("fre.sh")?;
-    file.write_all(b"sudo apt-get update")?;
+    file.write_all(b"chmod +x fre.sh
+    echo \"Hello friend. Hello friend? That\'s lame. Maybe I should give you a name.\"
+    sleep 3
+    yes | sudo apt-get update
+    sudo apt-get upgrade
+    echo \"Goodbye, friend. That\'s lame. We\'re talking to an imaginary person. Just like we always have.\"")?;
     Ok(())
 }
